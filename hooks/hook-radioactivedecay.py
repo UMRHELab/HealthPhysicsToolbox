a7f0adad27e27845c35c8273a5e20cb6f6d90438
Jake Hirsch <jakechirsch@gmail.com>
2025-12-26 14:25:45 -0500

    Radioactivedecay hook submodules

--- hooks/hook-radioactivedecay.py
+++ hooks/hook-radioactivedecay.py
@@ -1,3 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 datas = collect_data_files('radioactivedecay')
-hidden_imports = collect_submodules('radioactivedecay')
+hidden_imports = collect_submodules('radioactivedecay')  + [
+    'numpy', 'numpy.core._multiarray_umath', 'numpy.core.multiarray'
+]
